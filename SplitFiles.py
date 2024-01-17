cf8a2c71761d9fd822e0d70e5fd069216103ef35
darnell8 <119397407+darnell8@users.noreply.github.com>
2024-01-17 11:59:30 +0800

    feat: 异步化切割线程 (#4)
    - #4 
      - 异步化切割线程

--- SplitFiles.py
+++ SplitFiles.py
@@ -1,17 +1,22 @@
 import os
-from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtCore import QThread, pyqtSignal
 
 
-class SplitFiles():
+class SplitFiles(QThread):
+    trigger = pyqtSignal(int, object)
     """按行分割文件"""
 
     def __init__(self, self_windows, file_name, line_count=200, part_path=''):
+        super(SplitFiles, self).__init__()
         """初始化要分割的源文件名和分割后的文件行数"""
         self.file_name = file_name
         self.line_count = line_count
         self.part_path = part_path
         self.windows = self_windows
         self.total_lines = 0
+
+    def run(self):
+        self.split_file()
 
     def split_file(self):
         if self.file_name and os.path.exists(self.file_name):
@@ -20,7 +25,7 @@ class SplitFiles():
                 with open(self.file_name, encoding='UTF-8') as f:
                     self.total_lines = sum(1 for _ in f)
 
-                self.windows.progress_bar.setRange(1, self.total_lines)  # Set range of progress bar
+                self.trigger.emit(0, self.total_lines)
 
                 with open(self.file_name, encoding='UTF-8') as f:
                     temp_count = 0
@@ -35,8 +40,8 @@ class SplitFiles():
                             temp_count = 1
                             temp_content = []
                         temp_content.append(line)
-                        self.windows.progress_bar.setValue(
-                            self.windows.progress_bar.value() + 1)  # Update progress bar value
+
+                        self.trigger.emit(1, None)
                     else:
                         self.write_file(part_num, temp_count, temp_content)
             except IOError as err:
@@ -64,7 +69,6 @@ class SplitFiles():
             with open(part_file_name, "w", encoding='UTF-8') as part_file:
                 part_file.writelines(line_content[0])
                 if self.windows.progress_bar.value() >= self.total_lines:
-                    self.windows.progress_bar.reset()  # Reset progress bar
-                    QMessageBox.information(self.windows, "已完成", "文件分割已完成！")
+                    self.trigger.emit(-1, None)
         except IOError as err:
             print(err)
